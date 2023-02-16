75be1fbf45297485da9cfcfb9ed74ff686931197
Alex <alex@codingcat.dev>
2023-02-16 13:51:50 -0500

    add user login

--- example-blog/src/env.d.ts
+++ example-blog/src/env.d.ts
@@ -2,8 +2,8 @@
 /// <reference types="astro/client" />
 
 interface ImportMetaEnv {
-  readonly APPWRITE_ENDPOINT: string;
-  readonly APPWRITE_PROJECT_ID: string;
-  readonly APPWRITE_DB_ID: string;
-  readonly APPWRITE_COLLECTION_ID: string;
+  readonly PUBLIC_APPWRITE_ENDPOINT: string;
+  readonly PUBLIC_APPWRITE_PROJECT_ID: string;
+  readonly PUBLIC_APPWRITE_DB_ID: string;
+  readonly PUBLIC_APPWRITE_COLLECTION_ID: string;
 }
--- example-blog/src/lib/appwrite.ts
+++ example-blog/src/lib/appwrite.ts
@@ -1,17 +1,77 @@
-import { Client, Databases, Models } from 'appwrite';
+import { Account, AppwriteException, Client, Databases, ID, Models } from 'appwrite';
+import { atom, WritableAtom } from 'nanostores';
 
+/** Setup */
+export const appwriteClient = new Client()
+    .setEndpoint(import.meta.env.PUBLIC_APPWRITE_ENDPOINT) // Your API Endpoint
+    .setProject(import.meta.env.PUBLIC_APPWRITE_PROJECT_ID); // Your project ID
+
+/** Database */
 export interface BlogPost extends Models.Document {
     title: string;
     date: string;
     description: string;
     content: string;
     slug: string;
+    cover: string;
 }
 
-export interface BlogPostList extends Models.DocumentList<BlogPost>{}
-
-export const appwriteClient = new Client()
-    .setEndpoint(import.meta.env.APPWRITE_ENDPOINT) // Your API Endpoint
-    .setProject(import.meta.env.APPWRITE_PROJECT_ID); // Your project ID
-
+export interface BlogPostList extends Models.DocumentList<BlogPost> { }
 export const appwriteDatabases = new Databases(appwriteClient);
+
+/** Account */
+export const appwriteAccount = new Account(appwriteClient);
+export const isLoggedIn: WritableAtom<undefined | Models.Session> = atom(undefined);
+
+// Check for session
+appwriteAccount.getSession('current').then(function (response) {
+    isLoggedIn.set(response);
+}, function (error) {
+    isLoggedIn.set(undefined)
+})
+
+export const login = async (email: string, password: string) => {
+    try {
+        const session = await appwriteAccount.createEmailSession(email, password);
+            isLoggedIn.set(session);
+            window.location.href = '/account';
+    } catch (error) {
+        const appwriteError = error as AppwriteException;
+        alert(appwriteError.message)
+    }
+}
+
+export const logout = async () => {
+    try {
+        const session = isLoggedIn.get();
+        if(session?.$id){
+        await appwriteAccount.deleteSession(session?.$id);
+            isLoggedIn.set(undefined);
+            window.location.href = '/';
+        }
+    } catch (error) {
+        const appwriteError = error as AppwriteException;
+        alert(appwriteError.message)
+    }
+}
+
+export const register = async (email: string, password: string) => {
+    try {
+        await appwriteAccount.create(ID.unique(), email, password);
+        const session = await appwriteAccount.createEmailSession(email, password);
+        isLoggedIn.set(session);
+        window.location.href = '/account';
+    } catch (error) {
+        const appwriteError = error as AppwriteException;
+        alert(appwriteError.message)
+    }
+}
+
+export const account = async () => {
+    try {
+        return appwriteAccount.get();
+    } catch (error) {
+        const appwriteError = error as AppwriteException;
+        alert(appwriteError.message)
+    }
+}
