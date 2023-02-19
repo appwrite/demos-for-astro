c7784ea04f147f91f98bb90e0cb93529e61501eb
Alex <alex@codingcat.dev>
2023-02-19 17:34:59 -0500

    include crud

--- example-blog/astro.config.mjs
+++ example-blog/astro.config.mjs
@@ -5,6 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+	output: 'server',
 	site: 'https://example.com',
 	integrations: [mdx(), sitemap()],
 });
